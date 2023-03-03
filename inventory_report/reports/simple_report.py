6fa6e76f6db08f7d8f3039785557cda92613daa7
Icaro Joel <icaromoura.programador@gmail.com>
2023-03-03 20:56:23 -0300

    feat: implement req 02

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,30 +1,24 @@
+from datetime import date
+from collections import Counter
+
+
 class SimpleReport:
     @staticmethod
     def generate(item_list) -> str:
-        datas_fabricacao = []
-        datas_validade = []
+        # https://realpython.com/python-counter/
+        company = Counter(item['nome_da_empresa'] for item in item_list)
+        data_fabricacao = item_list[0]['data_de_fabricacao']
+        data_validade = item_list[0]['data_de_validade']
 
         for item in item_list:
-            datas_fabricacao.append(item['data_de_fabricacao'])
-            datas_validade.append(item['data_de_validade'])    
-    
+            if item['data_de_fabricacao'] > data_fabricacao:
+                data_fabricacao = item['data_de_fabricacao']
+
+            if data_validade < str(date.today()):
+                data_validade = item['data_de_validade']
+
         return f'''
-Data de fabricação mais antiga: {item_list[0]['data_de_fabricacao']}
-Data de validade mais próxima: {item_list[0]['data_de_validade']}
-Empresa com mais produtos: {item_list[0]['nome_da_empresa']}
+Data de fabricação mais antiga: {data_fabricacao}
+Data de validade mais próxima: {data_validade}
+Empresa com mais produtos: {company}
         '''
-
-
-# print(SimpleReport.generate(
-#     [
-#         {
-#             "id": 1,
-#             "nome_do_produto": "CADEIRA",
-#             "nome_da_empresa": "Forces of Nature",
-#             "data_de_fabricacao": "2022-04-04",
-#             "data_de_validade": "2023-02-09",
-#             "numero_de_serie": "FR48",
-#             "instrucoes_de_armazenamento": "Conservar em local fresco"
-#         }
-#    ]
-# ))
