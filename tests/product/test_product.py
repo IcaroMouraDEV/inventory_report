e0ce7a9cffce32afc852b1ea4000a2a25f82f9bb
Icaro Joel <icaromoura.programador@gmail.com>
2023-03-03 15:23:42 -0300

    feat: implement req 01

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -5,15 +5,15 @@ def test_cria_produto():
     product = Product(
         id=1,
         nome_da_empresa='Moura Inc',
-        nome_do_produto='Mouse gamer Moura Mx-9000',
+        nome_do_produto='Mouse Mx-9000',
         data_de_fabricacao='03/03/2023',
         data_de_validade='03/03/2024',
         numero_de_serie='000001',
-        instrucoes_de_armazenamento='Cuidado com a peça usb',
+        instrucoes_de_armazenamento='Cuidado com usb',
     )
-    
+
     # https://www.programiz.com/python-programming/methods/built-in/repr
-    str = 'O produto Mouse gamer Moura Mx-9000 fabricado em 03/03/2023 por Moura Inc'
-    str2 = 'com validade até 03/03/2024 precisa ser armazenado Cuidado com a peça usb'
+    str = 'O produto Mouse Mx-9000 fabricado em 03/03/2023 por Moura Inc'
+    str2 = 'com validade até 03/03/2024 precisa ser armazenado Cuidado com usb.'
 
     assert repr(product) == f'{str} {str2}'
