c0578c38dc608891e39f1a66813946fb9a688edf
Icaro Joel <icaromoura.programador@gmail.com>
2023-03-04 19:34:39 -0300

    feat: implement req 08

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,18 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        id=1,
+        nome_da_empresa='Moura Inc',
+        nome_do_produto='Mouse Mx-9000',
+        data_de_fabricacao='03/03/2023',
+        data_de_validade='03/03/2024',
+        numero_de_serie='000001',
+        instrucoes_de_armazenamento='Cuidado com usb',
+    )
+
+    str = 'O produto Mouse Mx-9000 fabricado em 03/03/2023 por Moura Inc com'
+    str2 = 'validade até 03/03/2024 precisa ser armazenado Cuidado com usb'
+
+    assert repr(product) == f'{str} {str2}.'
