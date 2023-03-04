fed708683f0b6a2fdacac3e01b1f3d710f98903f
Icaro Joel <icaromoura.programador@gmail.com>
2023-03-04 12:35:30 -0300

    simplify

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,15 +1,27 @@
+import csv
+import json
+import xml.etree.ElementTree as ET
+
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
-import csv
 
 class Inventory:
     @staticmethod
     def import_data(path: str, type: str):
+        data_type = path.split('.')[1]
         with open(path) as file:
-            reader = list(csv.DictReader(file))
-
+            if data_type == 'csv':
+                data = list(csv.DictReader(file))
+            if data_type == 'json':
+                data = json.load(file)
+            if data_type == 'xml':  # https://docs.python.org/3/library/xml.etree.elementtree.html
+                xml = ET.parse(file).getroot()
+                data = [
+                    {item.tag: item.text for item in product} for product in xml
+                ]
+            
             report = SimpleReport if type == 'simples' else CompleteReport
 
-            return report.generate(reader)
+            return report.generate(data)
 
 print(Inventory.import_data('inventory_report/data/inventory.csv', 'simples'))
