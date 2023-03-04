0a475d4d226dc87aec75486a905b97c9e9ac9d43
Icaro Joel <icaromoura.programador@gmail.com>
2023-03-03 23:09:06 -0300

    feat: implement req 02

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -24,28 +24,15 @@ class SimpleReport:
                 data_validade = item['data_de_validade']
 
         return (data_fabricacao, data_validade)
-
-        
+ 
     @staticmethod
     def generate(item_list) -> str:
+        data = SimpleReport.__get_fab_and_val_date(item_list)
         company = SimpleReport.__get_most_commom_company(item_list)
-        data_fabricacao, data_validade = SimpleReport.__get_fab_and_val_date(item_list)
+        data_fabricacao, data_validade = data
 
         return f'''
 Data de fabricação mais antiga: {data_fabricacao}
 Data de validade mais próxima: {data_validade}
 Empresa com mais produtos: {company}
         '''
-
-
-print(SimpleReport.generate([
-    {
-       "id": 1,
-       "nome_do_produto": "CADEIRA",
-       "nome_da_empresa": "Forces of Nature",
-       "data_de_fabricacao": "2022-04-04",
-       "data_de_validade": "2023-02-09",
-       "numero_de_serie": "FR48",
-       "instrucoes_de_armazenamento": "Conservar em local fresco"
-     }
-]))
