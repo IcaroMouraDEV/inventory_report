ba237e54231b065e2c882193fa1fd56aa891e5d7
Icaro Joel <icaromoura.programador@gmail.com>
2023-03-03 23:48:01 -0300

    feat: implement req 02

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -7,18 +7,18 @@ class SimpleReport:
     def __get_most_commom_company(item_list):
         company = Counter(item['nome_da_empresa'] for item in item_list)
         return company.most_common(1)[0][0]
-    
+
     @staticmethod
     def __get_fab_and_val_date(item_list):
         data_fab = min([item['data_de_fabricacao'] for item in item_list])
         data_validade = min([
-            item['data_de_validade'] for item in item_list if datetime.strptime(
-                item['data_de_validade'],
+            itm['data_de_validade'] for itm in item_list if datetime.strptime(
+                itm['data_de_validade'],
                 '%Y-%m-%d') > datetime.now()
         ])
-        
+
         return (data_fab, data_validade)
- 
+
     @staticmethod
     def generate(item_list) -> str:
         data = SimpleReport.__get_fab_and_val_date(item_list)
