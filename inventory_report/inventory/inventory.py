1ce704ba4d046c608f4b19f31a9fbe5e20f4148a
Icaro Joel <icaromoura.programador@gmail.com>
2023-03-04 14:58:34 -0300

    fix: linter

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -5,6 +5,7 @@ import xml.etree.ElementTree as ET
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
+
 class Inventory:
     @staticmethod
     def import_data(path: str, type: str):
@@ -14,10 +15,11 @@ class Inventory:
                 data = list(csv.DictReader(file))
             if data_type == 'json':
                 data = json.load(file)
-            if data_type == 'xml':  # https://docs.python.org/3/library/xml.etree.elementtree.html
+            if data_type == 'xml':
+                # https://docs.python.org/3/library/xml.etree.elementtree.html
                 xml = ET.parse(file).getroot()
                 data = [
-                    {item.tag: item.text for item in product} for product in xml
+                    {itm.tag: itm.text for itm in product} for product in xml
                 ]
             
             report = SimpleReport if type == 'simples' else CompleteReport
