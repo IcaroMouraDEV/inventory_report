0cd7b932ac0406183aea18ab44fe29dfb83213db
Icaro Joel <icaromoura.programador@gmail.com>
2023-03-03 23:02:37 -0300

    feat: implement req 02

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,12 +1,15 @@
-from datetime import date, timedelta
+from datetime import date
 from collections import Counter
 
 
 class SimpleReport:
     @staticmethod
-    def generate(item_list) -> str:
-        # https://realpython.com/python-counter/
+    def __get_most_commom_company(item_list):
         company = Counter(item['nome_da_empresa'] for item in item_list)
+        return company.most_common(1)[0][0]
+    
+    @staticmethod
+    def __get_fab_and_val_date(item_list):
         data_fabricacao = item_list[0]['data_de_fabricacao']
         data_validade = item_list[0]['data_de_validade']
 
@@ -16,13 +19,33 @@ class SimpleReport:
 
             if (
                 data_validade > str(date.today())
-                and str(date.today() + timedelta(days=999))
+                and item['data_de_validade'] < data_validade
             ):
                 data_validade = item['data_de_validade']
 
-            # print(data_validade > str(date.today()))
+        return (data_fabricacao, data_validade)
+
+        
+    @staticmethod
+    def generate(item_list) -> str:
+        company = SimpleReport.__get_most_commom_company(item_list)
+        data_fabricacao, data_validade = SimpleReport.__get_fab_and_val_date(item_list)
+
         return f'''
 Data de fabricação mais antiga: {data_fabricacao}
 Data de validade mais próxima: {data_validade}
-Empresa com mais produtos: {company.most_common(1)[0][0]}
+Empresa com mais produtos: {company}
         '''
+
+
+print(SimpleReport.generate([
+    {
+       "id": 1,
+       "nome_do_produto": "CADEIRA",
+       "nome_da_empresa": "Forces of Nature",
+       "data_de_fabricacao": "2022-04-04",
+       "data_de_validade": "2023-02-09",
+       "numero_de_serie": "FR48",
+       "instrucoes_de_armazenamento": "Conservar em local fresco"
+     }
+]))
